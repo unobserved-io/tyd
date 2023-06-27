66575f36a984f65039346288440d86b60cb63ae9
RIcky Kresslein <ricky@lakoliu.com>
2023-06-27 12:12:06 +0200

    Rename ContentView to HomeView

--- Tyd/HomeView.swift
+++ Tyd/HomeView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import SwiftData
 
-struct ContentView: View {
+struct HomeView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
     
@@ -55,6 +55,6 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    HomeView()
         .modelContainer(for: Item.self, inMemory: true)
 }
