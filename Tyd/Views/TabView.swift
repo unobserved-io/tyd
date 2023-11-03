394d628ab8450d59855a010378aa13d58bed471c
Ricky Kresslein <ricky@unobserved.io>
2023-11-03 09:55:31 +0100

    Add tab view

---
+++ Tyd/Views/TabView.swift
@@ -0,0 +1,18 @@
+//
+//  TabView.swift
+//  Tyd
+//
+//  Created by Ricky Kresslein on 6/29/23.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabView()
+}
