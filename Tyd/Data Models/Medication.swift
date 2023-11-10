9a58f288e8150e3c1b69a94eb1eba5c5ab823a7d
Ricky Kresslein <ricky@unobserved.io>
2023-11-10 09:25:51 +0100

    Add UUID to Medication model

--- Tyd/Data Models/Medication.swift
+++ Tyd/Data Models/Medication.swift
@@ -10,11 +10,13 @@ import SwiftData
 
 @Model
 final class Medication {
+    var id: UUID
     var name: String
     var time: Date
     var dose: String
 
     init() {
+        self.id = UUID()
         self.name = ""
         self.time = .now
         self.dose = ""
