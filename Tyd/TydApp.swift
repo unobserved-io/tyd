d8803389da1506252a3ec4df2614bb733ec0efdc
Ricky Kresslein <ricky@unobserved.io>
2023-11-10 11:18:04 +0100

    Remove unused legacy model declaration

--- Tyd/TydApp.swift
+++ Tyd/TydApp.swift
@@ -10,21 +10,6 @@ import SwiftUI
 
 @main
 struct TydApp: App {
-//    let modelContainer: ModelContainer
-        
-//        init() {
-//            do {
-//                modelContainer = try ModelContainer(for:
-//                    AppData.self,
-//                    Day.self,
-//                    Medication.self,
-//                    TamponTimer.self
-//                )
-//            } catch {
-//                fatalError("Could not initialize ModelContainer")
-//            }
-//        }
-    
     var body: some Scene {
         WindowGroup {
             LoadingView()
