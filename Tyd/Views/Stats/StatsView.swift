b9b719964a7971255815aed6f30017b673d741ce
Ricky Kresslein <ricky@unobserved.io>
2023-12-17 11:58:29 +0100

    Make StatsView visually appealing

--- Tyd/Views/Stats/StatsView.swift
+++ Tyd/Views/Stats/StatsView.swift
@@ -9,34 +9,173 @@ import SwiftUI
 
 struct StatsView: View {
     @Environment(Stats.self) private var stats
+    let dualInnerBoxColor: Color = .white.opacity(0.5)
+    let boxColor: Color = .accent.opacity(0.2)
     let monthDay: DateFormatter = {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "MMM d"
         return dateFormatter
     }()
+    @State var bleedingDay: Int = 1
     
     var body: some View {
-        VStack {
-            Text("Last Period")
-//                .bold()
+        ScrollView{
+            VStack {
+                // Last Period
+                VStack {
+                    Text("Last Period")
+                        .frame(alignment: .center)
+                        .font(.title2)
+                    Text("\(monthDay.string(from: stats.lastPeriodStart ?? .distantPast)) - \(monthDay.string(from: stats.lastPeriodEnd ?? .distantFuture))")
+                        .frame(alignment: .center)
+                        .font(.system(size: 55))
+                        .foregroundStyle(.accent)
+                }
+                .padding(.top)
+                
+                // Average period
+                VStack {
+                    VStack {
+                        HStack(alignment: .lastTextBaseline) {
+                            Text(String(stats.avgPeriodLength ?? 0))
+                                .font(.system(size: 55))
+                                .foregroundStyle(.accent)
+                            Text(stats.avgPeriodLength != 1 ? "days" : "day")
+                        }
+                        Text("Average period")
+                            .font(.title2)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                }
+                .background(boxColor)
+                .clipShape(RoundedRectangle(cornerRadius: 15))
                 .frame(alignment: .center)
-                .font(.title2)
-            Text("\(monthDay.string(from: stats.lastPeriodStart ?? .distantPast)) - \(monthDay.string(from: stats.lastPeriodEnd ?? .distantFuture))")
+                
+                // Average PMS days/cycle
+                VStack {
+                    Text(String(format: "%.1f", stats.avgPmsDaysPerCycle))
+                        .font(.system(size: 55))
+                        .foregroundStyle(.accent)
+                    Text("Average PMS days per cycle")
+                        .font(.title2)
+                }
+                
+                // Average bleeding on day x
+                VStack {
+                    VStack {
+                        HStack {
+                            Button {
+                                leftArrowPressed()
+                            } label: {
+                                Image(systemName: "chevron.left").bold()
+                            }
+                            .padding(.leading)
+                            Spacer()
+                            Text(String(format: "%.1f", (stats.avgBleedingByDay[bleedingDay] ?? 0)))
+                                .font(.system(size: 55))
+                                .foregroundStyle(.accent)
+                            Spacer()
+                            Button {
+                                rightArrowPressed()
+                            } label: {
+                                Image(systemName: "chevron.right").bold()
+                            }
+                            .padding(.trailing)
+                        }
+                        
+                        Text("Average bleeding on day \(String(bleedingDay))")
+                            .font(.title2)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                }
+                .background(boxColor)
+                .clipShape(RoundedRectangle(cornerRadius: 15))
                 .frame(alignment: .center)
-                .font(.largeTitle)
-                .foregroundStyle(.accent)
-            Spacer()
-            Text("Last period: \(monthDay.string(from: stats.lastPeriodEnd ?? .distantPast))") // Date
-            Text("Avg period length: \(stats.avgPeriodLength ?? 0)") // Number of days
-            Text("Avg PMS days per cycle: \(String(format: "%.1f", stats.avgPmsDaysPerCycle))") // # of days
-            Text("Avg bleeding on day one: \(String(format: "%.1f", (stats.avgBleedingByDay[1] ?? 0)))")
-            Text("Avg cycle: \(stats.avgCycle ?? 0)")
-            
-            Text("Days using tyd: \(stats.daysUsingTyd)")
-            Text("Total period days: \(stats.totalPeriodDays)")
-            Text("Total PMS days: \(stats.totalPmsDays)")
-            
-            Text("Tooltip to record more data to see more stats")
+                
+                // Average cycle
+                VStack {
+                    HStack(alignment: .lastTextBaseline) {
+                        Text(String(stats.avgCycle ?? 0))
+                            .font(.system(size: 55))
+                            .foregroundStyle(.accent)
+                        Text(stats.avgCycle != 1 ? "days" : "day")
+                    }
+                    Text("Average cycle")
+                        .font(.title2)
+                }
+                
+                // Tyd usage
+                VStack {
+                    VStack {
+                        VStack {
+                            Text("Days using Tyd")
+                                .font(.title2)
+                            Text(String(stats.daysUsingTyd))
+                                .font(.system(size: 55))
+                                .foregroundStyle(.accent)
+                        }
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                    }
+                    .background(dualInnerBoxColor)
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                    .frame(alignment: .center)
+                    .padding(.horizontal)
+                    .padding(.top)
+                    HStack {
+                        VStack {
+                            VStack {
+                                Text(String(stats.totalPeriodDays))
+                                    .font(.system(size: 55))
+                                    .foregroundStyle(.accent)
+                                Text("Period days")
+                                    .font(.title2)
+                            }
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                        }
+                        .background(dualInnerBoxColor)
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                        .frame(alignment: .center)
+                        .padding(.leading)
+                        .padding(.bottom)
+                        VStack {
+                            VStack {
+                                Text(String(stats.totalPmsDays))
+                                    .font(.system(size: 55))
+                                    .foregroundStyle(.accent)
+                                Text("PMS Days")
+                                    .font(.title2)
+                            }
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                        }
+                        .background(dualInnerBoxColor)
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                        .frame(alignment: .center)
+                        .padding(.trailing)
+                        .padding(.bottom)
+                    }
+                }
+                .background(.accent.opacity(0.5))
+                .clipShape(RoundedRectangle(cornerRadius: 15))
+                .frame(alignment: .center)
+            }
+            .padding()
+        }
+    }
+    
+    func leftArrowPressed() {
+        if bleedingDay > 1 {
+            bleedingDay -= 1
+        }
+    }
+    
+    func rightArrowPressed() {
+        if bleedingDay < stats.avgBleedingByDay.count {
+            bleedingDay += 1
         }
     }
 }
