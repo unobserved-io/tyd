4e598d1228da9821850f0f718af313de1a136092
Ricky Kresslein <ricky@unobserved.io>
2023-12-22 10:28:22 +0100

    Only show stats when there is something to show. Otherwise show message that there is more to come

--- Tyd/Views/Stats/StatsView.swift
+++ Tyd/Views/Stats/StatsView.swift
@@ -16,6 +16,7 @@ struct StatsView: View {
         dateFormatter.dateFormat = "MMM d"
         return dateFormatter
     }()
+    @State private var showingMoreStatsMessage: Bool = false
 
     @State var bleedingDay: Int = 1
     
@@ -23,88 +24,99 @@ struct StatsView: View {
         ScrollView {
             VStack {
                 // Last Period
-                VStack {
-                    Text("Last Period")
-                        .frame(alignment: .center)
-                        .font(.title2)
-                    Text("\(monthDay.string(from: stats.lastPeriodStart ?? .distantPast)) - \(monthDay.string(from: stats.lastPeriodEnd ?? .distantFuture))")
-                        .frame(alignment: .center)
-                        .font(.system(size: 55))
-                        .foregroundStyle(.accent)
+                if stats.lastPeriodStart != nil && stats.lastPeriodEnd != nil {
+                    VStack {
+                        Text("Last Period")
+                            .frame(alignment: .center)
+                            .font(.title2)
+                        Text("\(monthDay.string(from: stats.lastPeriodStart ?? .distantPast)) - \(monthDay.string(from: stats.lastPeriodEnd ?? .distantFuture))")
+                            .frame(alignment: .center)
+                            .font(.system(size: 55))
+                            .foregroundStyle(.accent)
+                    }
+                    .padding(.top)
                 }
-                .padding(.top)
                 
                 // Average period
-                VStack {
+                if stats.avgPeriodLength ?? 0 != 0 {
                     VStack {
-                        HStack(alignment: .lastTextBaseline) {
-                            Text(String(stats.avgPeriodLength ?? 0))
-                                .font(.system(size: 55))
-                                .foregroundStyle(.accent)
-                            Text(stats.avgPeriodLength != 1 ? "days" : "day")
+                        VStack {
+                            HStack(alignment: .lastTextBaseline) {
+                                Text(String(stats.avgPeriodLength ?? 0))
+                                    .font(.system(size: 55))
+                                    .foregroundStyle(.accent)
+                                Text(stats.avgPeriodLength != 1 ? "days" : "day")
+                            }
+                            Text("Average period")
+                                .font(.title2)
                         }
-                        Text("Average period")
-                            .font(.title2)
+                        .frame(maxWidth: .infinity)
+                        .padding()
                     }
-                    .frame(maxWidth: .infinity)
-                    .padding()
+                    .background(boxColor)
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                    .frame(alignment: .center)
                 }
-                .background(boxColor)
-                .clipShape(RoundedRectangle(cornerRadius: 15))
-                .frame(alignment: .center)
                 
                 // Average PMS days/cycle
-                VStack {
-                    Text(String(format: "%.1f", stats.avgPmsDaysPerCycle))
-                        .font(.system(size: 55))
-                        .foregroundStyle(.accent)
-                    Text("Average PMS days per cycle")
-                        .font(.title2)
+                // Show only if there is an average cycle, otherwise this will be incorrect
+                if stats.avgCycle ?? 0 != 0 {
+                    VStack {
+                        Text(String(format: "%.1f", stats.avgPmsDaysPerCycle))
+                            .font(.system(size: 55))
+                            .foregroundStyle(.accent)
+                        Text("Average PMS days per cycle")
+                            .font(.title2)
+                    }
                 }
                 
                 // Average bleeding on day x
-                VStack {
+                if stats.avgCycle ?? 0 != 0 {
                     VStack {
-                        HStack {
-                            Button {
-                                leftArrowPressed()
-                            } label: {
-                                Image(systemName: "chevron.left").bold()
+                        VStack {
+                            HStack {
+                                Button {
+                                    leftArrowPressed()
+                                } label: {
+                                    Image(systemName: "chevron.left").bold()
+                                }
+                                .padding(.leading)
+                                Spacer()
+                                Text(String(format: "%.1f", stats.avgBleedingByDay[bleedingDay] ?? 0))
+                                    .font(.system(size: 55))
+                                    .foregroundStyle(.accent)
+                                Spacer()
+                                Button {
+                                    rightArrowPressed()
+                                } label: {
+                                    Image(systemName: "chevron.right").bold()
+                                }
+                                .padding(.trailing)
                             }
-                            .padding(.leading)
-                            Spacer()
-                            Text(String(format: "%.1f", stats.avgBleedingByDay[bleedingDay] ?? 0))
-                                .font(.system(size: 55))
-                                .foregroundStyle(.accent)
-                            Spacer()
-                            Button {
-                                rightArrowPressed()
-                            } label: {
-                                Image(systemName: "chevron.right").bold()
-                            }
-                            .padding(.trailing)
+                            
+                            Text("Average bleeding on day \(String(bleedingDay))")
+                                .font(.title2)
                         }
-                        
-                        Text("Average bleeding on day \(String(bleedingDay))")
-                            .font(.title2)
+                        .frame(maxWidth: .infinity)
+                        .padding()
                     }
-                    .frame(maxWidth: .infinity)
-                    .padding()
+                    .background(boxColor)
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                    .frame(alignment: .center)
                 }
-                .background(boxColor)
-                .clipShape(RoundedRectangle(cornerRadius: 15))
-                .frame(alignment: .center)
                 
                 // Average cycle
-                VStack {
-                    HStack(alignment: .lastTextBaseline) {
-                        Text(String(stats.avgCycle ?? 0))
-                            .font(.system(size: 55))
-                            .foregroundStyle(.accent)
-                        Text(stats.avgCycle != 1 ? "days" : "day")
+                if stats.avgCycle ?? 0 != 0 {
+                    VStack {
+                        HStack(alignment: .lastTextBaseline) {
+                            Text(String(stats.avgCycle ?? 0))
+                                .font(.system(size: 55))
+                                .foregroundStyle(.accent)
+                            Text(stats.avgCycle != 1 ? "days" : "day")
+                        }
+                        Text("Average cycle")
+                            .font(.title2)
                     }
-                    Text("Average cycle")
-                        .font(.title2)
                 }
                 
                 // Tyd usage
@@ -163,6 +175,12 @@ struct StatsView: View {
                 .background(.accent.opacity(0.5))
                 .clipShape(RoundedRectangle(cornerRadius: 15))
                 .frame(alignment: .center)
+                
+                if stats.lastPeriodStart == nil || stats.lastPeriodEnd == nil || stats.avgPeriodLength ?? 0 == 0 || stats.avgCycle ?? 0 != 0 {
+                    Text("You will see more stats once you have recorded more data.")
+                        .bold()
+                        .padding(.top)
+                }
             }
             .padding()
         }
