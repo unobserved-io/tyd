2583126e755619006d88d3dde50415f9d4bdec0c
Ricky Kresslein <accounts@kressle.in>
2024-05-27 08:18:41 +0200

    Add close button to About sheet

--- Tyd/Views/Settings/AboutView.swift
+++ Tyd/Views/Settings/AboutView.swift
@@ -8,43 +8,56 @@
 import SwiftUI
 
 struct AboutView: View {
-    let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+    @Environment(\.dismiss) var dismiss
     @Environment(\.colorScheme) var colorScheme
 
+    let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+    
     var body: some View {
-        ScrollView {
-            VStack(spacing: 40.0) {
-                VStack {
-                    Image("TydLogo")
-                        .foregroundStyle(.accent)
-                        .padding(.bottom, 10.0)
-                    HStack(alignment: .lastTextBaseline) {
-                        Text("by")
-                            .padding(.trailing, 8.0)
-                            .opacity(0.4)
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 40.0) {
+                    VStack {
+                        Image("TydLogo")
+                            .foregroundStyle(.accent)
+                            .padding(.bottom, 10.0)
+                        HStack(alignment: .lastTextBaseline) {
+                            Text("by")
+                                .padding(.trailing, 8.0)
+                                .opacity(0.4)
 
-                        Image(
-                            uiImage: colorScheme == .light
-                                ? .unobservedText.resize(targetSize: CGSize(width: 220, height: 17.0))
-                                : .unobservedTextWhite.withRenderingMode(.alwaysOriginal).resize(targetSize: CGSize(width: 220, height: 17.0))
-                        )
+                            Image(
+                                uiImage: colorScheme == .light
+                                    ? .unobservedText.resize(targetSize: CGSize(width: 220, height: 17.0))
+                                    : .unobservedTextWhite.withRenderingMode(.alwaysOriginal).resize(targetSize: CGSize(width: 220, height: 17.0))
+                            )
+                        }
+                    }
+
+                    Text("""
+                    Thank you for using Tyd. We understand the trust it takes to put your personal information into an app, which is why we make privacy a promise.
+
+                    Tyd is created by Unobserved. As the name suggests, Unobserved creates privacy-first software for people who don't believe their data should be spread across the internet, often under the control of people and companies who use it for sketchy purposes.
+
+                    No accounts are required for any Unobserved apps, no data is collected, even for analytics, and we never show ads to monetize our apps. This is app design as it should be.
+                    """)
+
+                    Text("Questions, suggestions, or concerns? Email [support@unobserved.io](mailto:support@unobserved.io)")
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+
+                    Text("Version \(appVersion ?? "unknown")")
+                }
+                .toolbar {
+                    ToolbarItem {
+                        Button {
+                            dismiss()
+                        } label: {
+                            Label("Close", systemImage: "xmark.circle")
+                        }
                     }
                 }
-
-                Text("""
-                Thank you for using Tyd. We understand the trust it takes to put your personal information into an app, which is why we make privacy a promise.
-
-                Tyd is created by Unobserved. As the name suggests, Unobserved creates privacy-first software for people who don't believe their data should be spread across the internet, often under the control of people and companies who use it for sketchy purposes.
-
-                No accounts are required for any Unobserved apps, no data is collected, even for analytics, and we never show ads to monetize our apps. This is app design as it should be.
-                """)
-
-                Text("Questions, suggestions, or concerns? Email [support@unobserved.io](mailto:support@unobserved.io)")
-                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
-
-                Text("Version \(appVersion ?? "unknown")")
+                .padding()
             }
-            .padding()
         }
     }
 }
